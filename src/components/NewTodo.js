de9b7979b53ae92f004486e0a69f4802526847e6
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-10 01:35:35 +0700

    NewTodo contains text as key of its state

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const NewTodo = () => (
-  <div>
-    <input type="text" />
-    <button>Add</button>
-  </div>
-);
+class NewTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <button>Add</button>
+      </div>
+    );
+  }
+}
 
 export default NewTodo;
