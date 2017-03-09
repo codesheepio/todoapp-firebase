dc0450dba4a967a17ef1d359aacf071c8e1ac05e
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-09 20:24:39 +0700

    Add test NewTodo

---
+++ test/3_NewTodo.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NewTodo from '../src/components/NewTodo';
+
+describe('NewTodo', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NewTodo />);
+  });
+  it('renders <div> correctly', () => {
+    expect(wrapper.is('div')).toBe(true);
+  });
+
+  it('renders <input> with "text" type inside <div>', () => {
+    expect(wrapper.find('input').exists()).toBe(true);
+    expect(wrapper.find('input').first().prop('type')).toEqual('text');
+  });
+
+  it('renders <button> with "Add" text inside <div>', () => {
+    expect(wrapper.find('button').exists()).toBe(true);
+    expect(wrapper.find('button').first().text()).toBe('Add');
+  });
+});
