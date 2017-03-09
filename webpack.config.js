d9347584df40b32e21c6c3d1f47b5840419781cc
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-09 17:51:19 +0700

    Scaffold project with webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack/hot/only-dev-server',
+    path.resolve(__dirname, 'src', 'index.js'),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    hot: true,
+  },
+};
