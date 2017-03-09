5541447273512a6239acdea013870c5bf3450706
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-10 01:57:47 +0700

    Add config template

---
+++ src/config/index.template.js
@@ -0,0 +1,7 @@
+export default {
+  apiKey: '<API KEY>',
+  authDomain: '<DOMAIN>.firebaseapp.com',
+  databaseURL: 'https://<DATABASE DOMAIN>.firebaseio.com',
+  storageBucket: '<STORAGE DOMAIN>.appspot.com',
+  messagingSenderId: '<MESSAGING SENDER ID>',
+};
