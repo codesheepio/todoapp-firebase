bb6a6c8bf776a23620024c17ec070ef520ec207c
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-10 01:07:56 +0700

    Render TodoItem on browser

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoItem from './components/TodoItem';
+
+ReactDOM.render(
+  <TodoItem text="Feed Cat" />,
+  document.getElementById('react-root')
+);
