da1f86c4f9deceac83eb38f277a81609a658fd28
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-10 01:17:10 +0700

    Renders all TodoItems

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 
-const TodoList = ({ todos }) => <ul />;
+const TodoList = ({ todos }) => (
+  <ul>
+    {todos.map(todo => <TodoItem key={todo.id} {...todo} />)}
+  </ul>
+);
 
 export default TodoList;
